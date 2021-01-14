0bac280c0bf4b3abef4091a2400f1e9dc8c64f76
Danil <danil@MacBook-Pro-Vladilen.local>
2021-01-14 12:16:36 +0300

    Finish project config

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './module';
+import './scss/style.scss';
+console.log('work!');
+('use strict');
+
+describe('some test', () => {
+  it('should return a string value', (done) => {
+    return done();
+  });
+});
