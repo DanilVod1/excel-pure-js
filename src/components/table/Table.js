a0e8ae5f018777687921de14df26ab279cf04fe8
Danil <danvodopyanov@yandex.ru>
2021-01-28 11:01:59 +0300

    Complete base table logic

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,14 +1,16 @@
-import {createTable} from './table.template';
-import {ExcelComponent} from '../../core/ExcelComponent';
-import {resizeHandler} from './resize';
-import {shouldResize} from './functions';
+import {ExcelComponent} from '@core/ExcelComponent';
+import {$} from '@core/dom';
+import {createTable} from '@/components/table/table.template';
+import {resizeHandler} from '@/components/table/table.resize';
+import {isCell, matrix, shouldResize} from '@/components/table/table.functions';
+import {TableSelection} from '@/components/table/TableSelection';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
 
   constructor($root) {
     super($root, {
-      listeners: ['mousedown'],
+      listeners: ['mousedown', 'keydown'],
     });
   }
 
@@ -16,9 +18,47 @@ export class Table extends ExcelComponent {
     return createTable(100);
   }
 
+  prepare() {
+    this.selection = new TableSelection();
+  }
+
+  init() {
+    super.init();
+    const $cell = this.$root.find('[data-id="0:0"]');
+    this.selection.select($cell);
+  }
+
   onMousedown(event) {
     if (shouldResize(event)) {
       resizeHandler(this.$root, event);
+    } else if (isCell(event)) {
+      const $target = $(event.target);
+      if (event.shiftKey) {
+        const $cells = matrix($target, this.selection.current).map((id) =>
+          this.$root.find(`[data-id="${id}"]`),
+        );
+        this.selection.selectGroup($cells);
+      } else {
+        this.selection.select($target);
+      }
     }
+  }
+  onKeydown(event) {
+    const $target = $(event.target);
+    let values;
+    let $cell;
+    const keyboard = [];
+    keyboard.push(event.key);
+
+    keyboard.map((el) => {
+      if (event.key == el) {
+        console.log('ff');
+        event.preventDefault();
+        values = $target.calculateValueMove(el);
+        $cell = this.$root.findMovedElement(values);
+      }
+    });
+    this.selection.select($cell);
+    $cell.$el.focus();
   }
 }
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -3,10 +3,12 @@ import {DomListener} from './DomListener';
 export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners);
+    this.prepare();
   }
   toHTML() {
     return '';
   }
+  prepare() {}
   init() {
     this.initDOMListeners();
   }
